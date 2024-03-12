6ac1195feff12bbb6c6b576664ee6feb940753d5
x0m0n <andy.cator@gmail.com>
2024-03-12 16:12:53 -0700

    Create DependenciesChck.ps1

---
+++ DependenciesChck.ps1
@@ -0,0 +1,6 @@
+#Meant to run once at the beginning.
+#Checks for dependencies: ImageMagick (magick), 7-zip (7z), ffmpeg (ffmpeg)
+#Checks for $PATH to those dependencies
+#Create parameters file for repeated runs: archive password, archive size, files per folder, excluded extensions, included movie extensions
+
+#To be made.
